ffc7ff9639d0d3267e299fa48d23a8b2288e1e30
dbrp2008 <dbrp2008@gmail.com>
2026-04-15 11:24:24 +0400

    Time: 47 ms (61.95%) | Memory: 19.2 MB (66.76%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        cache = {}
+        def recur_fib(n):
+            if n in cache:
+                return cache[n]
+            if n < 2:
+                result = n
+            else:
+                result = recur_fib(n-1) + recur_fib(n-2)
+            cache[n] = result
+            return result
+        return recur_fib(n)
