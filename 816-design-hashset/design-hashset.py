bf85954534f0b17f113310acf302699f1afb4060
dbrp2008 <dbrp2008@gmail.com>
2026-04-24 10:59:49 +0400

    Time: 160 ms (21.76%) | Memory: 24.1 MB (91.58%) - LeetSync

---
+++ 816-design-hashset/design-hashset.py
@@ -0,0 +1,26 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.num_buckets = 5
+        self.buckets = [[] for i in range(self.num_buckets)]
+
+    def add(self, key: int) -> None:
+        index = key % self.num_buckets
+        if key not in self.buckets[index]:
+            self.buckets[index].append(key)
+
+    def remove(self, key: int) -> None:
+        index = key % self.num_buckets
+        if key in self.buckets[index]:
+            self.buckets[index].remove(key)
+
+    def contains(self, key: int) -> bool:
+        index = key % self.num_buckets
+        return key in self.buckets[index]        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
