42467150f42dc49210ed967daa89decdb2caac34
dbrp2008 <dbrp2008@gmail.com>
2026-04-26 10:33:38 +0400

    Time: 85 ms (21.46%) | Memory: 19.8 MB (9.91%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        hashmap = {}
+        for char in s:
+            if char in hashmap:
+                hashmap[char] += 1
+            else:
+                hashmap[char] = 1
+        for i, char in enumerate(s):
+            if hashmap[char] == 1:
+                return i
+        return -1
